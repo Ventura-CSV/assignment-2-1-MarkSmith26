8e7b8f72a00f0c40e68d09829f786647f748ba2e
MarkSmith26 <mark_smith26@my.vcccd.edu>
2023-08-25 16:08:21 -0700

    create main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    males = input('Please enter number of males: ')
+    females = input('Please enter number of females: ')
+    total_stu = int(males) + int(females)
+    m_perc = (int(males) / int(total_stu)) * 100
+    f_perc = (int(females) / int(total_stu)) * 100
+
+    print(f'The total number of students: \t {total_stu}')
+    print(f'The number of males and females: \t {int(males)} \t {int(females)}')
+    print(f'The percentage of males and females: \t {m_perc} \t {f_perc}') 
 
     """
     ########################################
